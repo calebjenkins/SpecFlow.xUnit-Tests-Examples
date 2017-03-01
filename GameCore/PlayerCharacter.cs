f1e3acfebc3a9a0e27fda23b85e385545ad4e274
rakeshkumartiwari <tiwarirakesh8@gmail.com>
2017-03-01 17:39:39 +0530

    Add a n new Scenario

---
+++ GameCore/PlayerCharacter.cs
@@ -0,0 +1,20 @@
+﻿namespace GameCore
+{
+    public class PlayerCharacter
+    {
+        public PlayerCharacter()
+        {
+            Health = 100;
+        }
+        public void Hit(int damage)
+        {
+            Health -= damage;
+            if (Health <= 0)
+            {
+                IsDead = true;
+            }
+        }
+        public int Health { get; private set; } 
+        public bool IsDead { get; set; }
+    }
+}
